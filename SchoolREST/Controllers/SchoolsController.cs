ef9746cab6c99eafa47cb314fa8edc54ecb0c8ff
Tan Ti Khuu <tak002@edu.zealand.dk>
2024-09-25 14:15:57 +0200

    Tilføjede StatusCodes og ActionResult<>

--- SchoolREST/Controllers/SchoolsController.cs
+++ SchoolREST/Controllers/SchoolsController.cs
@@ -17,38 +17,102 @@ namespace SchoolREST.Controllers
         }
 
         // GET: api/<TeachersController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet]
-        public IEnumerable<Teacher> Get()
+        public ActionResult<IEnumerable<Teacher>> Get()
         {
-            return _teachersRepository.Get();
+            IEnumerable<Teacher> teachers = _teachersRepository.Get();
+            if (teachers == null)
+            {
+                return NotFound();
+            }
+            return Ok(teachers);
         }
 
+
         // GET api/<TeachersController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("{id}")]
-        public Teacher Get(int id)
+        public ActionResult<Teacher> Get(int id)
         {
-            return _teachersRepository.GetById(id);
+            Teacher? teacher = _teachersRepository.GetById(id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+            return Ok(teacher);
         }
 
         // POST api/<TeachersController>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost]
-        public Teacher Post([FromBody] Teacher newTeacher)
+        public ActionResult<Teacher> Post([FromBody] Teacher newTeacher)
         {
-         return _teachersRepository.AddTeacher(newTeacher);
+            try
+            {
+                Teacher createdTeacher = _teachersRepository.AddTeacher(newTeacher);
+                return Created("/" + createdTeacher.Id, createdTeacher);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
 
         // PUT api/<TeachersController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpPut("{id}")]
-        public Teacher Put(int id, [FromBody] Teacher putTeacher)
+        public ActionResult<Teacher?> Put(int id, [FromBody] Teacher putTeacher)
         {
-            return _teachersRepository.Update(id, putTeacher);
+            try
+            {
+                Teacher? updatedTeacher = _teachersRepository.Update(id, putTeacher);
+                if (updatedTeacher == null)
+                {
+                    return BadRequest("Teacher not found");
+                }
+                return Ok(updatedTeacher);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // DELETE api/<TeachersController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpDelete("{id}")]
-        public Teacher Delete(int id)
+        public ActionResult<Teacher> Delete(int id)
         {
-            return _teachersRepository.Remove(id);
+            Teacher deletedTeacher = _teachersRepository.Remove(id);
+            if (deletedTeacher == null)
+            {
+                return NotFound();
+            }
+            return Ok(deletedTeacher);
         }
     }
 }
